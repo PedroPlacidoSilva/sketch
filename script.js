abf2db13f989e0edb1ada0d9c195544e59c7ff57
PedroPlacidoSilva <44819385+PedroPlacidoSilva@users.noreply.github.com>
2022-10-07 15:35:35 +0100

    Added function squares to create divs on html

---
+++ script.js
@@ -0,0 +1,16 @@
+function grid(squares) {
+  // Makes the Board to Sketch
+
+  for (let i = 0; i < squares * 2; i++) {
+    // Create a div for every square of the board
+
+    // Create a div tag
+    let square = document.createElement("div");
+    // Add a class to the div (Change an attribute)
+    square.classList = "square";
+    // Append div to the div "sketchBoard"
+    document.getElementById("sketchBoard").appendChild(square);
+  }
+}
+
+grid(16);
