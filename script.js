7e6be15911c65668337d0c27fdea6e76198ca49f
root <root@DFKFS-002.dfk.local>
2022-10-08 20:36:05 +0100

    Updated function grid to update the number of divs created when the slider moves

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function grid(squares) {
+  // Removes all div elements if they exist. Reset divs
+  const elements = document.getElementsByClassName("square");
+  // Delete div with index 0 if exists
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
+  }
   // Makes the Board to Sketch
-
   for (let i = 0; i < squares * 2; i++) {
     // Create a div for every square of the board
 
@@ -16,9 +21,10 @@ function grid(squares) {
 // Updates the value of the Slider
 
 let range = document.querySelector("input");
+// Sets the firt number of div's on upload
+grid(range.value);
 
 range.addEventListener("input", function () {
   document.getElementById("rangeValue").innerHTML = range.value;
+  grid(range.value);
 });
-
-grid(16);
